a06d650aa516d0f03e10f0faf51da6d82b54cc25
Alan <alanst@microsoft.com>
2016-07-23 03:39:37 -0700

    a

--- templates/matttest/v2/headnode.sh
+++ templates/matttest/v2/headnode.sh
@@ -1,4 +1,4 @@
 sudo su
-export JAVA_HOME=java-install-dir; export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME=/opt/java/jdk1.7.0_79; export PATH=$JAVA_HOME/bin:$PATH
 cd /opt/datasynapse
 ./server.sh start
