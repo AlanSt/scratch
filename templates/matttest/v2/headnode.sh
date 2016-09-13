4cb7ac0f407b2b153483a63e48b6f70bd92cbc02
Alan <alanst@microsoft.com>
2016-09-13 13:34:56 -0700

    a

--- templates/matttest/v2/headnode.sh
+++ templates/matttest/v2/headnode.sh
@@ -4,8 +4,8 @@ new=`hostname -f`
 export JAVA_HOME=/opt/java/jdk1.7.0_79; export PATH=$JAVA_HOME/bin:$PATH
 cd /opt/datasynapse
 ./server.sh stop
-sed -i 's/${old}/${new}/g' /opt/datasynapse/webapps/livecluster/WEB-INF/config/driver.properties
-sed -i 's/${old}/${new}/g' /opt/datasynapse/webapps/livecluster/WEB-INF/config/Config.xml.unix
-sed -i 's/${old}/${new}/g' /opt/datasynapse/webapps/livecluster/WEB-INF/config/installation.properties
-sed -i 's/${old}/${new}/g' /opt/datasynapse/webapps/livecluster/WEB-INF/driverInstall/driver.properties
+sed -i "s/${old}/${new}/g" /opt/datasynapse/webapps/livecluster/WEB-INF/config/driver.properties
+sed -i "s/${old}/${new}/g" /opt/datasynapse/webapps/livecluster/WEB-INF/config/Config.xml.unix
+sed -i "s/${old}/${new}/g" /opt/datasynapse/webapps/livecluster/WEB-INF/config/installation.properties
+sed -i "s/${old}/${new}/g" /opt/datasynapse/webapps/livecluster/WEB-INF/driverInstall/driver.properties
 ./server.sh start
