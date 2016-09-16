823f9e4eb649856f155be3f56b120c45e4d1625e
Alan <AlanSt@s-MacBook-Pro.local>
2016-09-16 12:21:09 -0700

    script cleanup

--- templates/matttest/v2/engine.sh
+++ templates/matttest/v2/engine.sh
@@ -1,7 +1,5 @@
 sudo su
-#dns=`hostname -d`
-#director=${1}.${dns}
 director=10.0.0.100
 cd /opt/DSEngine
-sudo ./configure.sh -s ${director}:8000 -l n
-sudo ./engine.sh start
+./configure.sh -s ${director}:8000 -l n
+./engine.sh start
